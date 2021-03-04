e0d82ced1566cb2445f0f9db084435798cf1d4eb
Imam MAhbir Afraz <70149305+Afraz-2007@users.noreply.github.com>
2021-03-04 19:50:17 +0600

    Update Main.java

--- Java/studentPromotionOrDemotion/Main.java
+++ Java/studentPromotionOrDemotion/Main.java
@@ -1,8 +1,19 @@
 package imam.Afraz;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Student newobj = new Student("Adnin", 2.2f);
+
+        Scanner s1 = new Scanner(System.in);
+
+        System.out.print("Student's name: ");
+        String name = s1.nextLine();
+
+        System.out.print("Student's gpa: ");
+        Float gpa = s1.nextFloat();
+
+        Student newobj = new Student(name, gpa);
+
         newobj.showInfo();
     }
 }
