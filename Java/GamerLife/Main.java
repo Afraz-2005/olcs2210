5c46d697ad8a1ce19528fa27af867022f44f57c2
Imam MAhbir Afraz <70149305+Afraz-2007@users.noreply.github.com>
2021-03-14 19:15:59 +0600

    Update Main.java

--- Java/GamerLife/Main.java
+++ Java/GamerLife/Main.java
@@ -1,9 +1,23 @@
 package valorant.game;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        ReadyPlayer game = new ReadyPlayer("Imam Mahbir Afraz", "Afraz2007");
+        Scanner s1 = new Scanner(System.in);
+
+        System.out.print("Full Name: ");
+        String fullname = s1.nextLine();
+
+        System.out.print("Username: ");
+        String username = s1.nextLine();
+
+        System.out.print("Subscription type: ");
+        String subscriptionType = s1.nextLine();
+
+        ReadyPlayer game = new ReadyPlayer(fullname, username);
+
         game.showInfo();
+        game.setSubscriptionType(subscriptionType);
         game.buyEliteCoins(20);
         game.playerTierTracker(2222);
         game.showInfo();
