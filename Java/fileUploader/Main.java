5380fe765177bcf061e03c42497c98a3e6b2e06a
Imam Mahbir Afraz <70149305+Afraz-2007@users.noreply.github.com>
2021-03-21 20:00:38 +0600

    Update Main.java

--- Java/fileUploader/Main.java
+++ Java/fileUploader/Main.java
@@ -1,8 +1,21 @@
-package com.company;
+package Test.test;
 
 public class Main {
     public static void main(String[] args) {
-        FileUploader obj1 = new FileUploader("hehe", "txt", 4);
-        obj1.
+        FileUploader obj1 = new FileUploader("hehe", "txt", 5);
+        obj1.ShowInfo();
+
+        FileUploader obj2 = new FileUploader("wakanda", "jpg", 3.5f);
+        obj2.ShowInfo();
+
+        FileUploader obj3 = new FileUploader("hehe", "txt", 2.9f);
+        obj3.ShowInfo();
+
+        FileUploader obj4 = new FileUploader("hehe", "txt", 6);
+        obj4.ShowInfo();
+
+        FileUploader obj5 = new FileUploader("hehe", "txt", 0);
+        obj5.ShowInfo();
+
     }
 }
